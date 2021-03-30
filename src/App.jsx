1c6cdd8b9bd1d62be5b419ebda8349334fdd8636
huangli <1833002417@qq.com>
2021-03-30 18:30:28 +0800

    feat： lazy demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component, lazy, Suspense} from 'react';
 import './App.css';
 
-const BatteryContext = React.createContext(60);
+const About  = lazy(()=> import(/* webpackChunkName:"about" */'./About.jsx')); //异步导入组件
 
+//ErrorBoundary
+//错误处理方式1：componentDidCatch
+//错误处理方式2: getDerivedStateFromError
 
-class Leaf extends Component {
-  static contextType = BatteryContext;
-  
-  render() {
-    const battery = this.context;
-    return (
-      <h1> Battery : {battery}</h1>
-      // <BatteryContext.Consumer>
-      //   { battery => <h1> Battery : {battery}</h1> }
-      // </BatteryContext.Consumer>
-    );
-  }
-}
-class Middle extends Component {
-  render() {
-    return <Leaf />
-  }
-}
 class App extends Component {
   state = {
-    battery: 60,
+    hasError: false
     
   }
+
+  //错误处理方式1
+  // componentDidCatch(){
+  //   this.setState({
+  //     hasError : true
+  //   })
+  // }
+
+  //错误处理方式2
+  static getDerivedStateFromError(){
+    return {
+      hasError : true
+    }
+
+  }
+
   render() {
-    const { battery } = this.state;
-    return (
-      <BatteryContext.Provider value={battery}>
-          <button type="button" onClick={() => this.setState({ battery: battery - 1 })}> 减少</button>
-         
-          <Middle />
-      </BatteryContext.Provider>
-    )
+    if(this.state.hasError){
+      return <div>Error</div>
+    }
+
+    /** Suspense 和 Lazy 配合使用*/
+    return (<div>
+      <Suspense fallback= { <div>loading</div>}>
+         <About></About>
+      </Suspense>
+    </div>
+    );
   }
 }
 
