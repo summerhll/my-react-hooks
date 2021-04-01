bd85588c9aefd1800e11e586b4ff4491f48744d0
huangli <1833002417@qq.com>
2021-04-01 17:53:17 +0800

    feat:pureComp[onent+memo

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,73 @@
-import React, { Component, lazy, Suspense} from 'react';
+// eslint-disable-next-line
+import React, { Component, PureComponent, memo } from 'react'; 
 import './App.css';
 
-const About  = lazy(()=> import(/* webpackChunkName:"about" */'./About.jsx')); //异步导入组件
 
-//ErrorBoundary
-//错误处理方式1：componentDidCatch
-//错误处理方式2: getDerivedStateFromError
+//方法一：使用Component和shouldComponentUpdate 来判断是否需要更新Foo组件
+// class Foo extends Component{
+//   shouldComponentUpdate(nextProps, nextState){
+//     if(nextProps.name === this.props.name){
+//       return false;
+//     }
+//     return true;
+//   }
+//   render(){
+//     console.log('Foo render...');
+//     return null;
+//   }
+// }
+
+//方法二：使用PureComponent 让Foo组件在数据修改后才更新
+// class Foo extends PureComponent{
+//   render(){
+//     console.log('Foo render...');
+//     return null;
+//   }
+// }
+
+//方法三: memo 用于函数式组件
+const Foo = memo(function Foo(props) {
+  console.log('Foo render...');
+  return <div>{props.person.age}</div>;
+})
+
+
 
 class App extends Component {
+
   state = {
-    hasError: false
-    
+    count: 0,
+    person: {
+      age: 1
+    }
+
   }
 
-  //错误处理方式1
-  // componentDidCatch(){
-  //   this.setState({
-  //     hasError : true
-  //   })
-  // }
-
-  //错误处理方式2
-  static getDerivedStateFromError(){
-    return {
-      hasError : true
-    }
+  callback = () => { //将callback写成类组件，绑定this
 
   }
 
   render() {
-    if(this.state.hasError){
-      return <div>Error</div>
-    }
+    const person = this.state.person
+    return (
+      <div>
+        <button onClick={() => {
+          person.age++;
+          this.setState({ person })  //这一句加上后Foo组件不会触发更新
+        }}>修改</button>
 
-    /** Suspense 和 Lazy 配合使用*/
-    return (<div>
-      <Suspense fallback= { <div>loading</div>}>
-         <About></About>
-      </Suspense>
-    </div>
+        {/* <Foo name = "Mike" ></Foo> */}
+
+        {/* 这一句加上后Foo组件会触发更新，因为每次cb都创建了一个新函数 */}
+        {/* <Foo name = "Mike" cb = { ()=> {}} ></Foo> */}
+
+
+        {/* 这一句加上后Foo组件不会触发更新*/}
+        {/* <Foo name = "Mike" cb = { this.callback} ></Foo> */}
+
+        {/* memo */}
+        <Foo person={person} ></Foo>
+      </div>
     );
   }
 }
